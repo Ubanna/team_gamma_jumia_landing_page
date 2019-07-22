b13d70381093561bf3449adf0bf5aed31681dd34
Ubanna <danekehu@gmail.com>
2019-07-22 14:51:00 +0000

    finished

---
+++ src/components/Profilefooter.js
@@ -0,0 +1,126 @@
+import React, {Component} from 'react'
+import './profile.css'
+import { Divider } from '@material-ui/core';
+
+class Profilefooter extends Component{
+    render(){
+        return(
+            <div className='Line'>
+                <div className='LineOne'>LET US HELP YOU
+                
+                <h5>Help Center<br></br>
+                    Contact us<br></br>
+                    How to shop on Jumia?<br></br>
+                    Delivery options and timelines<br></br>
+                   How to return a product on Jumia?<br></br>
+                   Corporate and bulk purchases<br></br>
+                   Jumia Bot (Beta version)</h5>
+                   <br></br>
+                   JOIN US ON
+                   <br/>
+                   <div className='Picture'>
+                   <img className='Images' src="/pics/FaceBookLogo (copy).png" 
+                   width='15%' height='10%' alt='image'/>
+                   <img className='Images' src="/pics/TwitterLogo (copy).png" 
+                   width='8%' height='10%' alt='image'/>
+                   <img className='Images' src="/pics/UTubeLogo (copy).png" 
+                   width='12%' height='10%' alt='image'/>
+                   <img className='Images' src="/pics/ChartLogo (copy).png" 
+                   width='8%' height='10%' alt='image'/>
+                   <img className='Images' src="/pics/InstagramLogo (copy).png"
+                    width='8%' height='10%'alt='image'/>
+                   <img className='Images' src="/pics/TwitterLogo (copy).png" 
+                    width='8%' height='10%' alt='image'/>
+                </div>
+               </div>
+
+                <div className='LineTwo'>ABOUT JUMIA
+                
+               <h5>About us<br></br>
+               Jumia careers<br></br>
+               Jumia Express<br></br>
+               Jumia Global<br></br>
+               Terms and Conditions<br></br>
+               Privacy policy<br></br>
+               Ramadan Shop<br></br>
+               Mobile Week<br></br>
+               </h5>
+               <br></br>
+               CONTACT US
+               <br></br>
+               <br></br>
+               <br></br>
+               01 888 1100/ 0700 600 0000
+              
+              </div>
+
+                <div className='LineThree'> MAKE MONEY WITH JUMIA
+                    <h5>Sell on Jumia<br></br>
+                    Become an Affiliate Partner<br></br>
+                    Become a Sales Consultant<br></br>
+                     Become a Jumia Vendor Service Provider<br></br>
+                     </h5>
+                     <br></br>
+                     <br></br>
+                     <h5>PAYMENT METHODS & DELIVERY PARTNERS</h5>
+                     <div>
+                     <img className='Images2' src="/pics/JumiaLogo2 (copy).jpg"
+                      width='20%' height='10%' alt='image'/>
+                      <img className='Images2' src="/pics/Master (copy).png"
+                      width='15%' height='10%' alt='image'/>
+                      <img className='Images2' src="/pics/VisaMaster (copy).png"
+                      width='20%' height='10%' alt='image'/>
+                      <img className='Images2' src="/pics/Verve (copy).jpg"
+                      width='10%' height='10%' alt='image'/>
+                     </div>
+                     
+                </div>
+
+                  <div className='LineFour'>JUMIA INTERNATIONAL
+                                 <h5>
+                                 Algeria_____Morocco<br></br>
+
+                                 Cameroon____Senegal<br></br>
+
+                                 Egypt_______Tanzania<br></br>
+
+                                 Ghana_______Tunisia<br></br>
+
+                                Ivory Coast___Uganda<br></br>
+                                Kenya
+                                </h5>
+                                <br/><br/> <br/><br/><br/>
+                            
+                                <div>
+                                <img className='Images3' src="/pics/Mcash (copy).png"
+                                 width='25%' height='10%' alt='image'/>
+                                <img className='Images3' src="/pics/QuickTeller (copy).png" 
+                                width='30%' height='10%' alt='image'/>
+                                <img className='Images3' src="/pics/DHL1 (copy).png" 
+                                width='15%' height='10%' alt='image'/>
+                                <img className='Images3' src="/pics/Max (copy).png"
+                                 width='10%' height='10%' alt='image'/>
+                                </div>                      
+                                </div> 
+                                <div className="mfooter">
+
+<div className="mobilefooter">
+<li className="mobileli"><a href="#" className="nomoreli2" >CONTACT</a></li>
+<li className="mobileli"><a href="#" className="nomoreli2">HELP</a></li>
+<li className="mobileli"><a href="#" className="nomoreli2">CONDITIONS</a></li>
+<li className="mobileli"><a href="#" className="nomoreli2">BECOME A SELLER</a></li>
+</div>
+<div className="lfooter">
+<p className="mobileli"><a href="#" className="nomoreli3">All Rights Reserved</a></p>
+</div>
+</div>
+                                
+         </div>
+ 
+
+        
+        )
+    }
+}
+
+export default Profilefooter;
